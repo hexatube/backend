a1018ad33000a5a69dab3fec1b54658c0b3e8035
Igor Gilmutdinov <bladoff@gmail.com>
2024-06-06 14:58:49 +0300

    ci: deploy workflow

--- config/prod.exs
+++ config/prod.exs
@@ -11,3 +11,24 @@ config :logger, level: :info
 
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
+
+config :hexatube, Hexatube.Repo,
+  username: "postgres",
+  password: "postgres",
+  hostname: "localhost",
+  database: "hexatube_dev",
+  stacktrace: true,
+  show_sensitive_data_on_connection_error: false,
+  pool_size: 10
+
+config :hexatube, HexatubeWeb.Endpoint,
+  # Binding to loopback ipv4 address prevents access from other machines.
+  # Change to `ip: {0, 0, 0, 0}` to allow access from other machines.
+  http: [ip: {0, 0, 0, 0}, port: 4000],
+  check_origin: false,
+  code_reloader: false,
+  debug_errors: true,
+  secret_key_base: "umm+s+CJYlAFwyMQuDbBS4V9+SICRrxVLvCHcpyzuyYlj26bEDcC5Hc5zTWMK2pW",
+  watchers: []
+
+config :hexatube, dev_routes: true
---
+++ rel/overlays/bin/migrate.bat
@@ -0,0 +1,1 @@
+call "%~dp0\hexatube" eval Hexatube.Release.migrate
